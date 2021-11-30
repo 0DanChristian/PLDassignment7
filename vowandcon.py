bca38017f5f246663944beae0bde97d5d5498bdf
Dan Christian <dansilog16@gmail.com>
2021-11-30 23:40:05 +0545

    welcome

---
+++ vowandcon.py
@@ -0,0 +1,8 @@
+# program to count vowel or consonant of the given word or sentence
+
+# introduction
+print("Welcome!")
+# ask for use
+user = input("Please enter your name: ")
+# again, welcome
+print(f"Again, welcome {user}!, in this program you can enter a word or a senctence and we'll count how many vowels and consonant there is!")
