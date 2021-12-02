3968de26b6a746f5ba9dc74d8085b7945e567c9a
Dan Christian <dansilog16@gmail.com>
2021-12-02 16:37:07 +0545

    finalized

--- vowandcon.py
+++ vowandcon.py
@@ -7,34 +7,27 @@ user = input("\nPlease enter your \033[1mname\033[0m: ")
 # again, welcome
 print(f"\nWelcome \033[0;30;47m{user}\033[0m!, in this program you can enter a word or a senctence and we'll count how many \033[0;32;47mwords\033[0m, \033[0;32;47mvowels\033[0m and \033[0;32;47mconsonant\033[0m there is!\n")
 
-userSentenceWord = input("\nPlease enter a word or a sentence!: ")
+inUser = input("\nPlease enter a word or a sentence!: ")
 
 # wordcount, vowels and consonant
 vowels = 0
 consonants = 0
-wordcount = 1
+wordcount = 0
 
 # call the lower function to avoid upper case letter
-userSentenceWord.lower() 
-for word in userSentenceWord:
-    if(word == 'a'or word == 'e'or word == 'i'or word == 'o'or word == 'u' ):
-           vowels=vowels + 1
-    elif word in ' ':
-        wordcount += 1
-    else:
-        consonants=consonants + 1
-    
-wordcount = userSentenceWord.split()
+inUser.lower() 
+for word in inUser:
+    if word.upper() in "AEIOU" :
+           vowels += 1
+           wordcount += 1
+    elif word.upper() in "BCDFGHJKLMNPQRXTVWXYZ":
+        consonants += 1
+        
+wordcount = inUser.split()
 
     
 # display
-print("\nThe number of \033[0;32;47mwords\033[0m in the given input is",wordcount, end='')
-print(", the number of \033[0;32;47mvowel/s\033[0m is",vowels, end='')
-print(" and the number of \033[0;32;47mconsonant/s\033[0m is",consonants,)
-
-print("\n\033[0;37;42mVowel/s\033[0m       : ", vowels)
-print("\033[0;37;42mConsonant/s\033[0m   : ", consonants)
-print("\033[0;37;42mWordcount\033[0m     : ", wordcount)
+print("\nWord/s     : ", len(wordcount), "\nVowel/s    : ", vowels, "\nConsonant/s: ", consonants)
 
 # end of program
 print(f"\nThank you \033[0;37;46m{user}\033[0m!\n")
