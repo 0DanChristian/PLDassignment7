79a965b5cfaf20b2255943e21336ae089b1f0f52
Dan Christian <dansilog16@gmail.com>
2021-12-02 17:25:37 +0545

    checkpoint

--- password.py
+++ password.py
@@ -9,44 +9,44 @@ print("\n\033[0;37;45mWelcome!\033[0m\n")
 # ask for user
 user = input("\nPlease enter your \033[1mname\033[0m: ")
 # welcome
-print(f"\nWelcome \033[0;30;47m{user}\033[0m!, this program will validate if your password is valid! \n")
+print(f"\n\033[0;36;47mWelcome {user}\033[0m!, this program will validate if your password is valid! \n")
 
 # import
 import re
 
 # define
 def validate_password():
-          password= input("Enter your desired password: ")
+          password= input("\033[0;30;47mEnter your desired password\033[0m: ")
           valid = True 
           while valid:  
                 # if password is less than 15 characters
               if len(password) <= 15: 
-                  print("\033[0;37;41mPassword must have 15 characters\033[0m")     
+                  print("\n\033[0;37;41mPassword must have 15 characters\033[0m")     
                   break
                 # if pasword doesn't contain numbers
               elif not re.search("[0-9]",password): 
-                  print("\033[0;37;41mPassword must have atleast one number\033[0m")
+                  print("\n\033[0;37;41mPassword must have atleast one number\033[0m")
                   break
                 # if password doesn't contain upper case letter
               elif not re.search("[A-Z]",password): 
-                  print("\033[0;37;41mPassword must have atleast one uppercase\033[0m")
+                  print("\n\033[0;37;41mPassword must have atleast one uppercase\033[0m")
                   break
                 # if password doesn't contain special character
               elif not re.search("[$#@]",password): 
-                  print("\033[0;37;41mPassword must have atleast one special character\033[0m")
+                  print("\n\033[0;37;41mPassword must have atleast one special character\033[0m")
                   break
                 # if contains blank space
               elif re.search("\s",password): 
-                  print("\033[0;37;41mYou cannot have blank spaces in your password...\033[0m")
+                  print("\n\033[0;37;41mYou cannot have blank spaces in your password...\033[0m")
                   break
                 # Valid password has been created
               else:
-                  print("The password you entered is valid!:>\033[0m")
+                  print("\n\033[032;40mThe password you entered is valid!:>\033[0m\n")
                   valid = False 
                   break
                 # invalid
           if valid:
-              print("\n\033[0;37;41mUnfortunately, the password you input is invalid\033[0m")
+              print("\n\033[0;37;41mUnfortunately, the password you input is invalid\033[0m\n")
               validate_password()
 
 validate_password()
