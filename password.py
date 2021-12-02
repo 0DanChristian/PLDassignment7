5c9f316b9dd28fc61ddd0cacdbea1e5ba17675cd
Dan Christian <dansilog16@gmail.com>
2021-12-02 14:58:07 +0545

    modify,studying

--- password.py
+++ password.py
@@ -11,4 +11,20 @@ user = input("\nPlease enter your \033[1mname\033[0m: ")
 # welcome
 print(f"\nWelcome \033[0;30;47m{user}\033[0m!, this program will validate if your password is valid! \n")
 
-# the code before was a reference from google, stil studying it
+upper, special, digit = 0, 0, 0
+password = input("Enter your password: ")
+if (len(password) >= 15):
+    for i in password:
+        for word in password.split():
+            if(word[0].isupper()):
+                upper += 1
+        if(i.isdigit()):
+            digit += 1
+        if(i == '@' or i == '$' or i == '_' or i == '#'):
+            special += 1
+else:
+    print("Password must be greater than 15 letters")
+if (upper >= 1 and special >= 1 and digit >= 1):
+    print("Valid Password")
+else:
+    print("Invalid Password")
